f042056e78624b9934ff7f651097a5f7f74cabc4
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-03-10 18:38:33 +0530

    created backend project structure

---
+++ backend/src/db/connectDb.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+async function connectDB() {
+    // console.log()
+  try {
+    const connectionObject = await mongoose.connect(
+      `${process.env.MONGODB_URI}/`
+    );
+    console.log(
+      `\n MongoDB connected. DB HOST: ${connectionObject.connection.host}`
+    );
+  } catch (error) {
+    console.error("MongoDB connection failed: ", error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
