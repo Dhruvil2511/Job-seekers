55300501c56616c1faa9b9e67d19744149261ef0
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-03-10 19:10:25 +0530

    implemented get all jobs endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,18 @@
 import express from "express";
-export const app = express();
+import cors from "cors";
+const app = express();
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "10mb" })); // to encode the url with its param
+app.use(express.static("public")); // to serve static files
+
+import jobRouter from "./routes/job.route.js";
+
+app.use("/api/v1/jobs", jobRouter);
+export { app };
