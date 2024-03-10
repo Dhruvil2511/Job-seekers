dc1aec1cc4d8948ff077a8b582b77aa22ba40dcf
Sufiyan Shaikh <sufiyan.m.shaikh248@gmail.com>
2024-03-11 00:44:11 +0530

    Created Login, Register, Logout

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { register, login, logout, getprofile, uploadresume } from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import singleUpload from "../middlewares/multer.js";
+
+const router = Router();
+
+router.route("/register").post(register);
+
+router.route("/login").post(login);
+
+router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticated ,getprofile);
+
+router.route("/uploadresume").put(isAuthenticated, singleUpload, uploadresume);
+
+
+export default router;
