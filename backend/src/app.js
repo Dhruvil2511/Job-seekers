7ce06bf5b63f48f339e4267d03e3b1bfb6e479d3
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-03-11 10:23:35 +0530

    changes in dev

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import ErrorMiddleware from "./middlewares/Error.js";
 const app = express();
 app.use(
   cors({
@@ -8,11 +10,17 @@ app.use(
   })
 );
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" })); // to encode the url with its param
 app.use(express.static("public")); // to serve static files
 
 import jobRouter from "./routes/job.route.js";
+import userRouter from "./routes/user.route.js";
 
 app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/user", userRouter);
+
 export { app };
+
+app.use(ErrorMiddleware)
