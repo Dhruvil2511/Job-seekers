f491391085ae1459ea60da002cd97838ab2799bd
Dhruv-Dave29 <dhruvdave2910@gmail.com>
2024-03-11 12:28:25 +0530

    job details page and other things

--- frontend/src/pages/jobDetails/JobDetails.jsx
+++ frontend/src/pages/jobDetails/JobDetails.jsx
@@ -1,40 +1,70 @@
 import axios from "axios";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
 const JobDetails = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    console.log(id);
-    const [indiJobDetails, setIndiJobDetails] = useState({});
-    
-    // const fetchJobDetails = async () => {
-    //     try {
-    //         const jobDetails = await axios.get(`http://localhost:6969/api/v1/jobs/get-job/${id}`, {
-    //             withCredentials: true,
-    //         });
-    //         console.log(jobDetails);
-    //         setIndiJobDetails(jobDetails.data);
+  const fetchJobDetails = async () => {
+    try {
+      const jobDetails = await axios.get(
+        `http://localhost:6969/api/v1/jobs/get-job/`,
+        {
+          withCredentials: true,
+          params: { jobid: id },
+        }
+      );
+      console.log(jobDetails);
+      setIndiJobDetails(jobDetails.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-    
-    // useEffect(() => {
-    //     fetchJobDetails();
-    // }, []);
-  return(<>
-    <div>Job Details</div>
-    <div>{indiJobDetails.title}</div>
-    <div>{indiJobDetails.description}</div>
-    <div>{indiJobDetails.location}</div>
-    <div>{indiJobDetails.salary}</div>
-    <div>{indiJobDetails.company}</div>
-    <div>{indiJobDetails.experience}</div>
-    <div>{indiJobDetails.skills}</div>
+  useEffect(() => {
+    fetchJobDetails();
+  }, []);
 
-
-  </>);
+  // console.log(id);
+  const [indiJobDetails, setIndiJobDetails] = useState({
+    requirements: [],
+    required_skills: [],
+  });
+  return (
+    <>
+      <div className="container py-20">
+        <div className="flex  items-center flex-col top-[10rem] md:top-[14rem]">
+          <div className="text-[2.5rem] md:text-[5rem]">Job Details</div>
+          <div className="flex gap-8 items-center yw-full md:w-[calc(100%-400px)]">
+            <div className="pl-10 outline-none border-none w-full ">
+              <h2>{indiJobDetails.company}</h2>
+              <h3>{indiJobDetails.location}</h3>
+              <h4>{indiJobDetails.description}</h4>
+              <h1>{indiJobDetails.title}</h1>
+              <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-4">
+                <h1>requirements :</h1>
+                {indiJobDetails.requirements.map((skill) => (
+                  <span>{skill}</span>
+                ))}
+              </div>
+              <div className="flex flex-col gap-4">
+                <span>required skills:</span>
+                <div className="flex gap-4">
+                {indiJobDetails.required_skills.map((skill) => (
+                  <div className="cursor-pointer rounded-3xl border-2  px-3 py-1 text-xs font-semibold transition hover:bg-white hover:bg-opacity-50 hover:text-black">
+                    <span>{skill}</span>
+                  </div>
+                ))}
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default JobDetails;
